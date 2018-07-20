794e539cad6fcb6d80753db697dda172b20b818c
VictoriaMaria1707 <38673421+VictoriaMaria1707@users.noreply.github.com>
2018-07-19 20:08:10 -0500

    ll

---
+++ Parcial_II/wwwroot/js/propietarios.js
@@ -0,0 +1,119 @@
+﻿class ClasePropietario {
+
+    constructor(Nombre1, Nombre2, Apellido1, Apellido2, Direccion, Correo, Telefono, accion) {
+        this.Nombre1 = Nombre1;
+        this.Nombre2 = Nombre2;
+        this.Apellido1 = Apellido1;
+        this.Apellido2 = Apellido2;
+        this.Direccion = Direccion;
+        this.Correo = Correo;
+        this.Telefono = Telefono;
+        this.accion = accion;
+    }
+
+    guardarpropietario(id) {
+        var nombre1 = this.Nombre1;
+        var nombre2 = this.Nombre2;
+        var apellido1 = this.Apellido1;
+        var apellido2 = this.Apellido2;
+        var direccion = this.Direccion;
+        var telefono = this.Telefono;
+        var correo = this.Correo;
+        var accion = this.accion;
+        if (id == '0') {
+            $.ajax(
+                {
+                    type: "POST",
+                    url: accion,
+                    data: {
+                        nombre1,
+                        nombre2,
+                        apellido1,
+                        apellido2,
+                        direccion,
+                        telefono,
+                        correo,},
+                    success: (respuesta) => {
+                        if (respuesta[0].code == 'save') {
+                            this.limpiarcajaspro();
+                        }
+                    }
+                });
+        }
+        else {
+            // codigo actualizar
+            $.ajax(
+                {
+                    type: "POST",
+                    url: accion,
+                    data: {
+                        nombre1,
+                        nombre2,
+                        apellido1,
+                        apellido2,
+                        direccion,
+                        telefono,
+                        correo,
+                        id
+                    },
+                    success: (respuesta) => {
+                        if (respuesta[0].code == 'save') {
+                            this.limpiarcajas();
+                        }
+                    }
+                });
+        }
+
+
+    }
+
+    listaindex() {
+        var accion = this.accion;
+        $.ajax({
+            type: "POST",
+            url: accion,
+            data: {},
+            success: (respuesta) => {
+                $.each(respuesta, (index, val) => {
+                    $("#Listapropietario").html(val[0]);
+                });
+            }
+        });
+    }
+
+    Cargapropi(PropietarioId) {
+        var accion = this.accion;
+
+        $.post(
+            accion,
+            { PropietarioId },
+            (respuesta) => {
+                console.log(respuesta);
+                document.getElementById('Nombre1').value = respuesta[0].Nombre1;
+                document.getElementById('Nombre2').value = respuesta[0].Nombre2;
+                document.getElementById('Apellido1').value = respuesta[0].Apellido1;
+                document.getElementById('Apellido2').value = respuesta[0].Apellido2;
+                document.getElementById('Direccion').value = respuesta[0].Direccion;
+                document.getElementById('Correo').value = respuesta[0].Correo;
+                document.getElementById('Telefono').value = respuesta[0].Telefono;
+                document.getElementById('PropietarioId').value = respuesta[0].PropietarioId;
+            }
+        );
+
+
+    }
+
+    limpiarcajaspro() {
+        document.getElementById('Nombre1').value = '';
+        document.getElementById('Nombre2').value = '';
+        document.getElementById('Apellido1').value = '';
+        document.getElementById('Apellido2').value = '';
+        document.getElementById('Direccion').value = '';
+        document.getElementById('Correo').value = '';
+        document.getElementById('Telefono').value = '';
+        document.getElementById('PropietarioId').value = '';
+        $('#IngresoPropietario').modal('hide');
+        listaindex();
+    }
+
+}
--- Parcial_II/wwwroot/js/site.js
+++ Parcial_II/wwwroot/js/site.js
@@ -1,1 +1,40 @@
 ﻿// Write your JavaScript code.
+
+
+
+
+
+
+var sitelistaindexpro = () => {
+    var accion = 'Propietarios/Controladorlistaindexpro';
+    var clspropie = new ClasePropietario('', '', '', '', '', '', '', accion);
+    clspropie.listaindex();
+}
+
+
+var grabaPropietario = () => {
+    var Nombre1 = document.getElementById('Nombre1').value;
+    var Nombre2 = document.getElementById('Nombre2').value;
+    var Apellido1 = document.getElementById('Apellido1').value;
+    var Apellido2 = document.getElementById('Apellido2').value;
+    var Direccion = document.getElementById('Direccion').value;
+    var Correo = document.getElementById('Correo').value;
+    var Telefono = document.getElementById('Telefono').value;
+    var PropietarioId = document.getElementById('PropietarioId').value;
+    if (PropietarioId === '') {
+        PropietarioId = '0';
+        var accion = 'Propietarios/ControladorGuardaPropietario';
+    } else {
+
+        var accion = 'Propietarios/ControladorEditapropi';
+    }
+    var graba = new ClasePropietario(Nombre1, Nombre2, Apellido1, Apellido2, Direccion, Correo, Telefono, accion);
+    graba.guardarpropietario(PropietarioId);
+
+}
+
+var Cargapropie = (PropietarioId) => {
+    var accion = 'Propietarios/ControladorUnpropi';
+    var clspropieta = new ClasePropietario('', '', '', '', '', '', '', accion);
+    clspropieta.Cargapropi(PropietarioId);
+}
